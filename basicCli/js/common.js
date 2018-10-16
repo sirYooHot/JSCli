ca705971f7261f58244c723e5265b9fb11f209a2
unknown <linktoyoohot@163.com>
2018-10-16 11:39:20 +0800

    面试技巧

--- basicCli/js/common.js
+++ basicCli/js/common.js
@@ -1,3 +1,22 @@
+// 封装一个 创建节点 的函数
+function createELe(eleName, txt){
+	var ele  = document.createElement(eleName);// 创建元素节点
+	var Txt = document.createTextNode(txt);// 创建文本节点
+	ele.appendChild(Txt);
+	return ele;
+}
+
+// 原生DOM操作没有直接插入节点之后：封装一个 插入目标节点之后 的函数
+function insertAfter(eNew, eTarget){
+	var eParent = eTarget.parentNode;// 获取父节点
+	if(eParent.lastChild == eTarget){// target是否为父节点中last-child
+		eParent.appendChild(eNew);						
+	}else{
+		eParent.insertBefore(eNew, eTarget.nextSibling);
+	}
+}
+
+
 // 创建xhr
 function createXHR(){
 		if(window.XMLHttpRequest){
